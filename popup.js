06f73ddf066102392bcb653e28ac0e6acddbc1ca
JaAv <117873327+JaavRJ@users.noreply.github.com>
2023-04-09 01:42:26 -0600

    v2.1

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+//Acciones de las alcadias
 var btn_abrir_coyo = document.getElementById("btn_abrir_coyo"),
     overlay_coyo = document.getElementById("overlay_coyo"),
     popup_coyo = document.getElementById("popup_coyo"),
@@ -13,6 +14,8 @@ cerrar_alca_coyo.addEventListener('click' ,function(){
     popup_coyo.classList.remove('active');
 });
 
+
+
 var btn_abrir_mh = document.getElementById("btn_abrir_mh"),
     overlay_mh = document.getElementById("overlay_mh"),
     popup_mh = document.getElementById("popup_mh"),
@@ -26,4 +29,22 @@ btn_abrir_mh.addEventListener('mousedown', function(){
 cerrar_alca_mh.addEventListener('click' ,function(){
     overlay_mh.classList.remove('active');
     popup_mh.classList.remove('active');
+});
+
+
+
+//Acciones de los Lugares
+var btn_l1_mh = document.getElementById("btn_l1_mh"),
+    overlay_l1_mh = document.getElementById("overlay_l1_mh"),
+    popup_l1_mh = document.getElementById("popup_l1_mh"),
+    cerrar_l1_mh = document.getElementById("cerrar_l1_mh");
+
+    btn_l1_mh.addEventListener('mousedown', function(){
+    overlay_l1_mh.classList.add('active');
+    popup_l1_mh.classList.add('active');
+});
+
+cerrar_l1_mh.addEventListener('click' ,function(){
+    overlay_l1_mh.classList.remove('active');
+    popup_l1_mh.classList.remove('active');
 });
