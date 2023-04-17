912fef99bbaf60e4e08a952f3f90b411f966afdb
Seebaastiaan <sebasjara1503@outlook.com>
2023-04-16 20:00:33 -0600

    1.4

---
+++ popup.js
@@ -0,0 +1,196 @@
+
+
+//Acciones de las alcadias
+var btn_abrir_coyo = document.getElementById("btn_abrir_coyo"),
+    overlay_coyo = document.getElementById("overlay_coyo"),
+    popup_coyo = document.getElementById("popup_coyo"),
+    cerrar_alca_coyo = document.getElementById("cerrar_alca_coyo");
+
+btn_abrir_coyo.addEventListener('mousedown', function(){
+    overlay_coyo.classList.add('active');
+    popup_coyo.classList.add('active');
+});
+
+cerrar_alca_coyo.addEventListener('click' ,function(){
+    overlay_coyo.classList.remove('active');
+    popup_coyo.classList.remove('active');
+});
+
+
+
+var btn_abrir_mh = document.getElementById("btn_abrir_mh"),
+    overlay_mh = document.getElementById("overlay_mh"),
+    popup_mh = document.getElementById("popup_mh"),
+    cerrar_alca_mh = document.getElementById("cerrar_alca_mh");
+
+btn_abrir_mh.addEventListener('mousedown', function(){
+    overlay_mh.classList.add('active');
+    popup_mh.classList.add('active');
+});
+
+cerrar_alca_mh.addEventListener('click' ,function(){
+    overlay_mh.classList.remove('active');
+    popup_mh.classList.remove('active');
+});
+
+
+
+//Acciones de los Lugares
+var btn_l1_mh = document.getElementById("btn_l1_mh"),
+    overlay_l1_mh = document.getElementById("overlay_l1_mh"),
+    popup_l1_mh = document.getElementById("popup_l1_mh"),
+    cerrar_l1_mh = document.getElementById("cerrar_l1_mh");
+
+    btn_l1_mh.addEventListener('mousedown', function(){
+    overlay_l1_mh.classList.add('active');
+    popup_l1_mh.classList.add('active');
+});
+
+cerrar_l1_mh.addEventListener('click' ,function(){
+    overlay_l1_mh.classList.remove('active');
+    popup_l1_mh.classList.remove('active');
+});
+
+
+var btn_l2_mh = document.getElementById("btn_l2_mh"),
+    overlay_l2_mh = document.getElementById("overlay_l2_mh"),
+    popup_l2_mh = document.getElementById("popup_l2_mh"),
+    cerrar_l2_mh = document.getElementById("cerrar_l2_mh");
+
+    btn_l2_mh.addEventListener('mousedown', function(){
+    overlay_l2_mh.classList.add('active');
+    popup_l2_mh.classList.add('active');
+});
+
+cerrar_l2_mh.addEventListener('click' ,function(){
+    overlay_l2_mh.classList.remove('active');
+    popup_l2_mh.classList.remove('active');
+});
+
+
+var btn_l3_mh = document.getElementById("btn_l3_mh"),
+    overlay_l3_mh = document.getElementById("overlay_l3_mh"),
+    popup_l3_mh = document.getElementById("popup_l3_mh"),
+    cerrar_l3_mh = document.getElementById("cerrar_l3_mh");
+
+    btn_l3_mh.addEventListener('mousedown', function(){
+    overlay_l3_mh.classList.add('active');
+    popup_l3_mh.classList.add('active');
+});
+
+cerrar_l3_mh.addEventListener('click' ,function(){
+    overlay_l3_mh.classList.remove('active');
+    popup_l3_mh.classList.remove('active');
+});
+
+
+var btn_l4_mh = document.getElementById("btn_l4_mh"),
+    overlay_l4_mh = document.getElementById("overlay_l4_mh"),
+    popup_l4_mh = document.getElementById("popup_l4_mh"),
+    cerrar_l4_mh = document.getElementById("cerrar_l4_mh");
+
+    btn_l4_mh.addEventListener('mousedown', function(){
+    overlay_l4_mh.classList.add('active');
+    popup_l4_mh.classList.add('active');
+});
+
+cerrar_l4_mh.addEventListener('click' ,function(){
+    overlay_l4_mh.classList.remove('active');
+    popup_l4_mh.classList.remove('active');
+});
+
+
+var btn_l5_mh = document.getElementById("btn_l5_mh"),
+    overlay_l5_mh = document.getElementById("overlay_l5_mh"),
+    popup_l5_mh = document.getElementById("popup_l5_mh"),
+    cerrar_l5_mh = document.getElementById("cerrar_l5_mh");
+
+    btn_l5_mh.addEventListener('mousedown', function(){
+    overlay_l5_mh.classList.add('active');
+    popup_l5_mh.classList.add('active');
+});
+
+cerrar_l5_mh.addEventListener('click' ,function(){
+    overlay_l5_mh.classList.remove('active');
+    popup_l5_mh.classList.remove('active');
+});
+
+
+var btn_l1_coyo = document.getElementById("btn_l1_coyo"),
+    overlay_l1_coyo = document.getElementById("overlay_l1_coyo"),
+    popup_l1_coyo = document.getElementById("popup_l1_coyo"),
+    cerrar_l1_coyo = document.getElementById("cerrar_l1_coyo");
+
+    btn_l1_coyo.addEventListener('mousedown', function(){
+    overlay_l1_coyo.classList.add('active');
+    popup_l1_coyo.classList.add('active');
+});
+
+cerrar_l1_coyo.addEventListener('click' ,function(){
+    overlay_l1_coyo.classList.remove('active');
+    popup_l1_coyo.classList.remove('active');
+});
+
+
+var btn_l2_coyo = document.getElementById("btn_l2_coyo"),
+    overlay_l2_coyo = document.getElementById("overlay_l2_coyo"),
+    popup_l2_coyo = document.getElementById("popup_l2_coyo"),
+    cerrar_l2_coyo = document.getElementById("cerrar_l2_coyo");
+
+    btn_l2_coyo.addEventListener('mousedown', function(){
+    overlay_l2_coyo.classList.add('active');
+    popup_l2_coyo.classList.add('active');
+});
+
+cerrar_l2_coyo.addEventListener('click' ,function(){
+    overlay_l2_coyo.classList.remove('active');
+    popup_l2_coyo.classList.remove('active');
+});
+
+
+var btn_l3_coyo = document.getElementById("btn_l3_coyo"),
+    overlay_l3_coyo = document.getElementById("overlay_l3_coyo"),
+    popup_l3_coyo = document.getElementById("popup_l3_coyo"),
+    cerrar_l3_coyo = document.getElementById("cerrar_l3_coyo");
+
+    btn_l3_coyo.addEventListener('mousedown', function(){
+    overlay_l3_coyo.classList.add('active');
+    popup_l3_coyo.classList.add('active');
+});
+
+cerrar_l3_coyo.addEventListener('click' ,function(){
+    overlay_l3_coyo.classList.remove('active');
+    popup_l3_coyo.classList.remove('active');
+});
+
+
+var btn_l4_coyo = document.getElementById("btn_l4_coyo"),
+    overlay_l4_coyo = document.getElementById("overlay_l4_coyo"),
+    popup_l4_coyo = document.getElementById("popup_l4_coyo"),
+    cerrar_l4_coyo = document.getElementById("cerrar_l4_coyo");
+
+    btn_l4_coyo.addEventListener('mousedown', function(){
+    overlay_l4_coyo.classList.add('active');
+    popup_l4_coyo.classList.add('active');
+});
+
+cerrar_l4_coyo.addEventListener('click' ,function(){
+    overlay_l4_coyo.classList.remove('active');
+    popup_l4_coyo.classList.remove('active');
+});
+
+
+var btn_l5_coyo = document.getElementById("btn_l5_coyo"),
+    overlay_l5_coyo = document.getElementById("overlay_l5_coyo"),
+    popup_l5_coyo = document.getElementById("popup_l5_coyo"),
+    cerrar_l5_coyo = document.getElementById("cerrar_l5_coyo");
+
+    btn_l5_coyo.addEventListener('mousedown', function(){
+    overlay_l5_coyo.classList.add('active');
+    popup_l5_coyo.classList.add('active');
+});
+
+cerrar_l5_coyo.addEventListener('click' ,function(){
+    overlay_l5_coyo.classList.remove('active');
+    popup_l5_coyo.classList.remove('active');
+});
